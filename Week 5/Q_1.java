94c3f841b35517b8595b4b35a6478df16609fc5a
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-02-21 19:49:11 +0530

    Programming Assignment Solutions

---
+++ Week 5/Q_1.java
@@ -0,0 +1,11 @@
+class A implements Number
+ {
+  
+	 int a;
+         int sq;
+	 public int findSqr(int a)
+  {
+		 sq=a*a;
+            return sq;		 
+  }
+}
---
+++ Week 5/Q_5.java
@@ -0,0 +1,23 @@
+              try
+            {
+	         switch (i)
+                  {
+		    case 0 : 
+			int Zero = 0; 
+			j = 92/ Zero; 		
+			break;
+		    case 1 : 
+			int b[ ] = null; 
+			j = b[0] ; 	
+			break;
+	           default:
+		       System.out.print("No exception");
+		    } 		
+	      }
+           			
+		catch (Exception e)
+              {		
+		   System.out.print(e) ;
+		}
+
+ 
