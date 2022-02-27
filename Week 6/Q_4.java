74bb9c7ab9d4cd7e6157ae3b904ac6d958fba908
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-02-27 22:00:44 +0530

    Programming Assignment Solution

---
+++ Week 6/Q_4.java
@@ -0,0 +1,12 @@
+public synchronized int sum()
+{
+  return(a+b);
+}
+
+public synchronized void inc() 
+   {
+    a++;
+    b++;
+   }
+}
+
---
+++ Week 6/Q_5.java
@@ -0,0 +1,7 @@
+
+  t1.start();  
+  t1.setName("Week 6 Assignment Q5");  
+
+
+  t2.start();  
+  t2.setName("Week 6 Assignment Q5 New");  
