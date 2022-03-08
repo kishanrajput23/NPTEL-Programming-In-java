e22db1f0dad6ff08cecf6a909e2a7cef6e882aac
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-03-08 18:04:35 +0530

    Programming Assignment Solutions

---
+++ Week 7/Q_4.java
@@ -0,0 +1,8 @@
+for(int a=0;a<s1.length();a++)
+     {  
+          char str=s1.charAt(a);
+          if(str=='e' || str=='E'|| str=='a' || str=='A' || str=='i' || str=='I' || str=='o' || str=='O' || str=='u' || str=='U') 
+	     {
+	       c=c+1;
+         }  
+     } 
---
+++ Week 7/Q_5.java
@@ -0,0 +1,7 @@
+   byte[] b=s1.getBytes();   
+	      
+                byte B = (byte) c;
+            	b[n]=B;
+		System.out.println(new String(b)); 
+	   }
+
